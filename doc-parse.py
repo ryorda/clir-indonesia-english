3856ef563e553a6acf735ac533abfd27ae686abb
Ryorda Triaptahadi <triaptahadiryorda@yahoo.co.id>
2018-03-21 13:59:39 +0700

    lowercase

--- doc-parse.py
+++ doc-parse.py
@@ -15,12 +15,29 @@ if (sys.argv[1]) == '1' :
 	# remove punctuation
 	for tuv in root.iter('tuv'):
 		text 	= tuv.find('seg').text.strip()
-		token = tokenizer.tokenize(text)
+		token = tokenizer.tokenize(text.lower())
 		text 	= ""
 
 		for t in token :
 			if (t not in [".", ",", ";", ":", "!", "?", "'", "\"", "-", "“", "”", "″", "(", ")", "[", "]", "{", "}", "/", "\\"]) :
 				text += " " + t
+
+		text 	= text.strip()
+		file_writer[tuv.get('lang')].write( text + "\n")
+
+
+	for k, f in file_writer.items() : 
+		f.close();
+
+elif (sys.argv[1]) == '2' :
+	# include punctuation
+	for tuv in root.iter('tuv'):
+		text 	= tuv.find('seg').text.strip()
+		token = tokenizer.tokenize(text.lower())
+		text 	= ""
+
+		for t in token :
+			text += " " + t
 
 		text 	= text.strip()
 		file_writer[tuv.get('lang')].write( text + "\n")
