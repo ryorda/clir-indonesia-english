6e8f4a69d6f9a20bf8a7f099513d6ef8855f4ef8
Ryorda Triaptahadi <triaptahadiryorda@yahoo.co.id>
2018-04-11 14:43:54 +0700

    add translation merger

---
+++ merge_translation.py
@@ -0,0 +1,27 @@
+import os
+import re
+
+dict_prefix = re.compile(r'$dictionary*')
+
+dictionary = {}
+
+for fname in os.listdir('dataset') :
+	if dict_prefix.match(fname) :
+		dname = fname.split('.')[0]
+		if dname not in dictionary :
+			dictionary[dname] = {}
+
+		fin = open('dataset/%s' % fname, 'r', encoding='utf-8')
+		for line in fin :
+			sp = line.strip().split("\t")
+			key = sp[0]
+			words = sp[1].split(",") if len(sp) > 1 else []
+			if key not in dictionary[dname] :
+				dictionary[dname][key] = set()
+			for w in words :
+				dictionary[dname][key].add(w)
+
+for key, vocabs in dictionary.items() :
+	f = open('dataset/%s.txt' % key, 'w')
+	for vocab, translation in vocabs.items() :
+		f.write("%s\t%s\n" % (vocab, ",".join(list(translation))) )
