57a45dbd235bc74a3e2d91eafbca4f9045186882
Ryorda Triaptahadi <triaptahadiryorda@yahoo.co.id>
2018-06-07 19:33:32 +0700

    update mode 5

--- doc_cleansing.py
+++ doc_cleansing.py
@@ -88,6 +88,33 @@ elif mode == 4 :
 			fout.write(text + "\n")
 		f.close()
 		fout.close()
+
+#lowercase + convert all non-alphanumeric to space + remove stopwords + stemmed
+elif mode == 5 :
+
+	for d in os.listdir('dataset/{0}/extracted_raw/en_id'.format(dataset)) :
+		idx = 0 # first is id, then if we found blank become english
+		fout = open('dataset/doc_query/clean/en_id/{0}/{1}'.format(mode, d), 'w', encoding='utf-8')
+		f = open('dataset/{0}/extracted_raw/en_id/{1}'.format(dataset, d), 'r', encoding='utf-8')
+		for line in f :
+			if not line :
+				idx += 1
+				fout.write("\n")
+				continue
+			text = line.strip().lower()
+			text = re_clean.sub(' ', text)
+			tokens = text.split()
+			text = []
+			for t in tokens :
+				if t not in sw[idx] :
+					t2 = stemmer[idx].stem(t)
+					text.append(t2)
+			text = " ".join(text)
+			fout.write(text + "\n")
+		f.close()
+		fout.close()
+
+
 else :
 	print("[ERROR] mode is invalid...")
 	exit()
