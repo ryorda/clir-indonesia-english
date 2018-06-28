0ea866c9a982adb22aa8d751b564bb27adf7a2b4
Ryorda Triaptahadi <triaptahadiryorda@yahoo.co.id>
2018-06-28 15:18:48 +0700

    single doc2vec test

--- doc2vec_test_single.py
+++ doc2vec_test_single.py
@@ -10,12 +10,21 @@ from nltk.corpus import stopwords
 import string
 from multiprocessing import Pool
 
-mode = int(sys.argv[1])
-limit = int(sys.argv[2])
+size = int(sys.argv[1])
+window = int(sys.argv[2])
+min_count = int(sys.argv[3])
+mode = int(sys.argv[4])
 
 en_doc = []
 en_stemmer = PorterStemmer()
 en_stops = set(stopwords.words('english'))
+
+def create_model(en_doc, size, window, mode, min_count) :
+	print('creating model_en_test_s' + str(size) + "_w" + str(window) + "_c" + str(min_count) + "_v" + str(mode)  + "...")
+	model_en = Doc2Vec(en_doc, size=size, window=window, dm=0, dbow_words=1, min_count=min_count, workers=4)
+	model_en.save('model/doc_query/model_en_test_s' + str(size) + "_w" + str(window) + "_c" + str(min_count) + "_v" + str(mode) + ".doc2vec")
+
+# add test case
 
 news = ['GH95', 'LAT94']
 regex_news = [None, re.compile('la[0-9]+')]
@@ -24,7 +33,7 @@ regex_docno2 = re.compile('</?DOCNO>')
 
 
 re_clean = re.compile(r'[^A-Za-z0-9]', re.M)
-word_freq = {}
+
 
 for i in range(len(news)) :
 	for d in os.listdir('news/%s/' % news[i]) :
@@ -38,11 +47,7 @@ for i in range(len(news)) :
 				elif '<DOCNO>' in line :
 					docno = regex_docno2.sub('', line.strip()).strip()
 				elif '</DOC>' in line :
-					# en_doc.append(LabeledSentence(words=words, tags=['news_' + docno]))
-					for w in words :
-						if w not in word_freq :
-							word_freq[w] = 0
-						word_freq[w] += 1
+					en_doc.append(LabeledSentence(words=words, tags=['news_' + docno]))
 				else :
 					text = line.strip().lower()
 					if mode == 1 :
@@ -81,8 +86,13 @@ for i in range(len(news)) :
 
 print("docs : %d" % len(en_doc))
 
-freqs = sorted(word_freq.items(), key=lambda kv: kv[1], reverse=True)
-for k, v in freqs :
-	if limit <= 0 :
-		break
-	print("{0} : {1}".format(k, v), flush=True)
+# pool = Pool(int(sys.argv[2]))
+
+# args = []
+# for size in [100, 200, 300, 400] :
+# 	for window in [1, 3, 5, 7] :
+# 		for min_count in [1, 5, 10, 20, 50] :
+			# args.append((size, window, mode, min_count))
+create_model(en_doc, size, window, mode, min_count)
+
+# pool.starmap(create_model, args)
